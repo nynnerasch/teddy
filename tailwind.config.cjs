efe306e4efe870136b6f144fcb34d635858eaab9
nynnerasch <nynne.andersen@yahoo.dk>
2022-12-14 12:59:11 +0100

    første commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      baggrundsfarve: "#F9D7DE",
+      tekstfarve: "##471C24",
+      hover: "#594ABF",
+      checkbox: "#A2D2FF",
+      hover2: "#FFB6E1",
+      knapfarve: "#FFFAF1",
+      tekstfelt: "#FEF1F1",
+      tekstfelt_skrift: "#AAAAAB",
+      afvis_cookies: "#F23333",
+      tillad_cookies: "#7EF089",
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
