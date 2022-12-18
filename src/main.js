0dec6eeb866fe0f546a448ac80e57a130f0c7a90
nynnerasch <nynne.andersen@yahoo.dk>
2022-12-18 21:49:13 +0100

    cookieboks

--- src/main.js
+++ src/main.js
@@ -9,3 +9,20 @@ inView(".inview", () => {
 inView(".inview2", () => {
   animate(".inview2", { x: [-2000, 0] }, { duration: 4 });
 });
+
+const acceptCookie = document.querySelector("#tillad");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieboks);
+denyCookie.addEventListener("click", fjernCookieboks);
+
+function fjernCookieboks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+
+/*
+if (sessionStorage.getItem("fjerncookieboks")) {
+cookieboks.classList.add("hidden");}
+*/
