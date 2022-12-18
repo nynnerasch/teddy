27cbd791da0d848925a5f44fb6b88cd4c1737905
nynnerasch <nynne.andersen@yahoo.dk>
2022-12-18 19:22:33 +0100

    animation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+inView(".inview", () => {
+  animate(".inview", { x: [-2000, 0] }, { duration: 2 });
+});
+
+inView(".inview2", () => {
+  animate(".inview2", { x: [-2000, 0] }, { duration: 4 });
+});
