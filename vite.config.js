ec2163544f0ef5e8133d349daa4122ade59564cf
nynnerasch <nynne.andersen@yahoo.dk>
2022-12-19 17:48:03 +0100

    jesus save my ass

--- vite.config.js
+++ vite.config.js
@@ -14,10 +14,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        shop: resolve(__dirname, "shop.html"),
-        findos: resolve(__dirname, "findos.html"),
-        produkt: resolve(__dirname, "produkt.html"),
-        sitemap: resolve(__dirname, "sitemap.html"),
+        // shop: resolve(__dirname, "shop.html"),
+        //  findos: resolve(__dirname, "findos.html"),
+        // produkt: resolve(__dirname, "produkt.html"),
+        // sitemap: resolve(__dirname, "sitemap.html"),
       },
     },
   },
